3b582d78eb4c288c40a15f0a8f0155f1b2586f39
PrafullaGarasia <100746487+PrafullaGarasia@users.noreply.github.com>
2023-02-24 17:58:56 -0500

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SettingsComponent } from './settings/settings.component';
+import { ItemsComponent } from './items/items.component';
+import { ItemDetailsComponent } from './item-details/item-details.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ConfirmationComponent } from './confirmation/confirmation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    LoginComponent,
+    DashboardComponent,
+    SettingsComponent,
+    ItemsComponent,
+    ItemDetailsComponent,
+    CartComponent,
+    CheckoutComponent,
+    ConfirmationComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/confirmation/confirmation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-confirmation',
+  templateUrl: './confirmation.component.html',
+  styleUrls: ['./confirmation.component.sass']
+})
+export class ConfirmationComponent {
+
+}
