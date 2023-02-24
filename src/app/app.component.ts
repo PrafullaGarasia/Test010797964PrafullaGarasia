a944982734dea902d8e5511bab225efa51a73f05
PrafullaGarasia <100746487+PrafullaGarasia@users.noreply.github.com>
2023-02-24 18:07:14 -0500

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { Box } from './box';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Test010797964PrafullaGarasia';
+  size: number;
+  description: string;
+  shippingLabelCode: string;
+  recipient: string;
+  weight: number;
+  shippingCompany: string;
+  boxes: Box[] = [];
+
+  ngOnInit() {
+    // Initialize the list with a default box
+    this.boxes.push({
+      size: 5,
+      description: 'Sample box containing some items',
+      shippingLabelCode: 'ABC123',
+      recipient: 'Your Name',
+      weight: 500,
+      shippingCompany: ''
+    });
+  }
+
+  addBox() {
+    // Create a new box object with the input values and push it to the list
+    const newBox: Box = {
+      size: this.size,
+      description: this.description,
+      shippingLabelCode: this.shippingLabelCode.toUpperCase(),
+      recipient: this.recipient ? this.recipient : 'Your Name',
+      weight: this.weight,
+      shippingCompany: this.shippingCompany
+    };
+    this.boxes.push(newBox);
+  }
 }
